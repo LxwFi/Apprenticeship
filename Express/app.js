321074015b02d2b89aba70655087d18ad51037af
LxwFi <lxwfi404@gmail.com>
2021-09-24 14:23:27 +0100

    Introduction to express

---
+++ Express/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+function coin() {
+    let a = "";
+    a = Math.floor(Math.random()* (3 - 1) ) + 1;
+    if (a == 1){
+        return "Tails";
+    } else if (a == 2) {
+        return "Heads";
+    }
+}
+const web = express();
+const port = 3000;
+web.use(express.static('public'));
+
+web.get("/now", (req, res) => {
+    const now = new Date();
+    res.send(now);
+});
+web.get("/flipcoin", (req, res) => {
+    let b = "";
+    res.send(coin(b));
+});
+web.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
+
